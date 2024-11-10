0f86ae94d5ba6d4bcc81e8d4ebd37068c38d0828
Farhan Kittur <kitturfarhan@gmail.com>
2024-11-09 19:27:44 -0500

    e

--- RocketryNanoCode/RocketryNanoCode.ino
+++ RocketryNanoCode/RocketryNanoCode.ino
@@ -1,11 +1,21 @@
 #include <Adafruit_DPS310.h>
 #include <math.h>
+#include <arduino-timer.h>
+#include <EEPROM.h>
 
 Adafruit_DPS310 dps;
 
+auto timer = timer_create_default(); // create a timer with default settings
+
+short locations[100];
+float totalTime = 0;
 float seaLevelPressure = 1013.25; // standard sea level pressure in hPa
 float initialHeight = 0;
-const int delayTime = 100;
+int index = 0;
+
+void addTime() {
+  totalTime += 0.1;
+}
 
 float calculateAltitude(float pressure) {
   return 44330 * (1.0 - pow(pressure / seaLevelPressure, 0.1903));
@@ -16,9 +26,8 @@ void setup() {
   while (!Serial) delay(10);
   //Just set the EEPROM to blank initially
   EEPROM.put(0,0.00f);
-
   Serial.println("DPS310 Pressure Sensor Test");
-
+  timer.every(100,addTime);
   if (!dps.begin_I2C()) {
     Serial.println("Failed to find DPS310 chip");
     while (1) delay(10);
@@ -33,19 +42,29 @@ void setup() {
   dps.getEvents(&temp_event, &pressure_event);
   initialHeight = calculateAltitude(pressure_event.pressure);
   Serial.println("Initial height set. Ready to measure relative height.");
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
   sensors_event_t temp_event, pressure_event;
+  Serial.println("TEST");
 
   if (dps.temperatureAvailable()) {
     dps.getEvents(&temp_event, &pressure_event);
-    
-    float currentAltitude = calculateAltitude(pressure_event.pressure);
+    short currentAltitude = (short)(calculateAltitude(pressure_event.pressure) * 100);
     float relativeHeight = currentAltitude - initialHeight;
     float max = 0.00f;
     EEPROM.get(0, max);
 
+  index++;
+
+  if (index < 100) {
+    locations[index] = currentAltitude;
+  }
+
+  if (index==100)
+    EEPROM.put(10,totalTime);
     // Serial.print("Temperature = ");
     // Serial.print(temp_event.temperature);
     // Serial.println(" Â°C");
@@ -60,14 +79,21 @@ void loop() {
 
     Serial.print("Max Height: ");
     Serial.print(max);
-    Serial.println(" meters");
+    Serial.println(" Centimeters");
+
+    Serial.print("Time: ");
+    Serial.print(totalTime);
+    Serial.println(" seconds");
+
+    Serial.print("Index: ");
+    Serial.println(index);
 
     Serial.println();
     if (currentAltitude > max) {
-      EEPROM.put(0,currentAltitude)
+      EEPROM.put(0,currentAltitude);
     }
   }
 
-  delay(delayTime);  // Wait for .1 seconds before next reading
+  delay(50);  // Wait for .1 seconds before next reading
 
 }
